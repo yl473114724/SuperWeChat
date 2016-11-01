13ad936f37512c07e83e173c5563c0dccab960dc
Yanglei <473114724@qq.com>
2016-11-02 03:43:15 +0800

    添加工具类以及添加已注册用户名的判断方法

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/CommonUtils.java
@@ -0,0 +1,31 @@
+package cn.ucai.superwechat.utils;
+
+import android.widget.Toast;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.SuperWeChatApplication;
+
+
+public class CommonUtils {
+    public static void showLongToast(String msg){
+        Toast.makeText(SuperWeChatApplication.applicationContext,msg,Toast.LENGTH_LONG).show();
+    }
+    public static void showShortToast(String msg){
+        Toast.makeText(SuperWeChatApplication.applicationContext,msg,Toast.LENGTH_SHORT).show();
+    }
+    public static void showLongToast(int rId){
+        showLongToast(SuperWeChatApplication.applicationContext.getString(rId));
+    }
+    public static void showShortToast(int rId){
+        showShortToast(SuperWeChatApplication.applicationContext.getString(rId));
+    }
+
+    public static void showMsgShortToast(int msgid) {
+        if (msgid > 0) {
+            showShortToast(SuperWeChatApplication.getInstance().getResources().getIdentifier(I.MSG_PREFIX_MSG + msgid, "string", SuperWeChatApplication.getInstance().getPackageName()));
+        } else {
+            showShortToast(R.string.msg_1);
+        }
+    }
+}
