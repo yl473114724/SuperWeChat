033c9c8148e17ee8e71f4d2c9b1b015de5e4f9a7
Yanglei <473114724@qq.com>
2016-11-02 22:49:59 +0800

    登录成功闪屏获取数据保存到内存中

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -2,23 +2,29 @@ package cn.ucai.superwechat.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import com.hyphenate.easeui.domain.User;
 
 import com.hyphenate.chat.EMClient;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.db.UserDao;
+import cn.ucai.superwechat.utils.L;
 
 /**
  * 开屏页
  *
  */
 public class SplashActivity extends BaseActivity {
+	private static final String TAG = SplashActivity.class.getSimpleName();
 
 	private static final int sleepTime = 2000;
+	SplashActivity mContext;
 
 	@Override
 	protected void onCreate(Bundle arg0) {
 		setContentView(R.layout.em_activity_splash);
 		super.onCreate(arg0);
+		mContext = this;
 	}
 
 	@Override
@@ -32,6 +38,11 @@ public class SplashActivity extends BaseActivity {
 					long start = System.currentTimeMillis();
 					EMClient.getInstance().groupManager().loadAllGroups();
 					EMClient.getInstance().chatManager().loadAllConversations();
+					UserDao dao = new UserDao(mContext);
+					User user = dao.getUser(EMClient.getInstance().getCurrentUser());
+					L.e(TAG,"user="+user);
+					SuperWeChatHelper.getInstance().setCurrentUser(user);
+
 					long costTime = System.currentTimeMillis() - start;
 					//wait
 					if (sleepTime - costTime > 0) {
