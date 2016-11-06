8a9980df4938e0d6d6304a13db770cdfa4932f06
Yanglei <473114724@qq.com>
2016-11-06 23:54:42 +0800

    使用设置flags的方法销毁栈

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SettingsActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SettingsActivity.java
@@ -45,6 +45,7 @@ import cn.ucai.superwechat.utils.MFGT;
  */
 @SuppressWarnings({"FieldCanBeLocal"})
 public class SettingsActivity extends BaseActivity implements OnClickListener {
+	private static final String TAG = SettingsActivity.class.getSimpleName();
 
 	/**
 	 * new message notification
@@ -379,10 +380,9 @@ public class SettingsActivity extends BaseActivity implements OnClickListener {
 					public void run() {
 						pd.dismiss();
 						// show login screen
-						ExitAppUtils.getInstance().exit();
-//						finish();
-						startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
-
+//						ExitAppUtils.getInstance().exit();
+						finish();
+						startActivity(new Intent(SettingsActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
 					}
 				});
 			}
