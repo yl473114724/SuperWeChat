45aebe92b931f78d97cd086fb723d715943b6103
Yanglei <473114724@qq.com>
2016-11-08 14:31:07 +0800

    自定义PopUpWindow

--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.AddContactActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.MainActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
@@ -89,5 +90,9 @@ public class MFGT {
     public static void gotoUserProfile(Activity context){
         startActivity(context, UserProfileActivity.class);
     }
+    public static void gotoAddFirent(Activity context){
+        startActivity(context, AddContactActivity.class);
+    }
+
 
 }
