4d91d8bfb8be8a6af1016814cb62144ea13681a0
Yanglei <473114724@qq.com>
2016-11-09 23:32:00 +0800

    重新user的toString方法

--- easeui/src/com/hyphenate/easeui/domain/User.java
+++ easeui/src/com/hyphenate/easeui/domain/User.java
@@ -108,9 +108,16 @@ public class User implements Serializable {
 
 	@Override
 	public String toString() {
-		return "UserAvatar [muserName=" + muserName + ", muserNick=" + muserNick + ", mavatarId=" + mavatarId
-				+ ", mavatarPath=" + mavatarPath + ", mavatarSuffix=" + mavatarSuffix + ", mavatarType=" + mavatarType
-				+ ", mavatarLastUpdateTime=" + mavatarLastUpdateTime + "]";
+		return "User{" +
+				"muserName='" + muserName + '\'' +
+				", muserNick='" + muserNick + '\'' +", mavatarId=" + mavatarId +
+				", mavatarPath='" + mavatarPath + '\'' +
+				", mavatarSuffix='" + mavatarSuffix + '\'' +
+				", mavatarType=" + mavatarType +
+				", mavatarLastUpdateTime='" + mavatarLastUpdateTime + '\'' +
+				", initialLetter='" + initialLetter + '\'' +
+				'}';
+
 	}
 
 	public String getInitialLetter() {
